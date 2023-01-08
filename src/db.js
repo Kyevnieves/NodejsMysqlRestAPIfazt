4478537d5555d156753151c2abc786975be67ccd
Kyevnieves <keyvernievess@gmail.com>
2023-01-08 17:03:12 -0430

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,17 @@
 const mysql2 = require("mysql2/promise");
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = require("./config");
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+  DB_PORT,
+} = require("./config");
 const pool = mysql2.createConnection({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
+  port: DB_PORT,
 });
 
 module.exports = pool;
